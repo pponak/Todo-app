9fc22c1b087f8b3b70315bbfad7c235289399ecd
stackovf <yamaxunkjh@163.com>
2026-09-15 23:27:35 +0800

    完成Day3并移除旧学习进度文件

--- main.py
+++ main.py
@@ -7,12 +7,28 @@ def save_tasks(tasks):
 
 # 函数：读取json文件中的任务
 def load_tasks():
+    # 尝试读取文件，如果文件不存在则返回空列表
     try:
         with open("tasks.json", "r", encoding="utf-8") as file:
             return json.load(file)
     except FileNotFoundError:
         # 如果文件不存在，则返回空列表
         return [] 
+
+# 获取用户输入的任务编号，判断是否合规，并转换为列表索引
+def get_task_index(tasks, action):
+    # 处理无效字符
+    try:
+        task_number = int(input(f"请输入要{action}的任务编号："))
+    except ValueError:
+        print("请输入有效的任务数字编号!!!")
+        return None
+    # 处理无效数字
+    if task_number < 1 or task_number > len(tasks):
+        print("没有这个任务编号!!!")
+        return None
+
+    return task_number - 1
 
 # 尝试读取上一次保存的任务
 tasks = load_tasks()
@@ -60,22 +76,12 @@ while True:
                 else:
                     status = "[ ]"
                 print(f"{index}. {status} {task['title']}")
+
+    # 完成任务
     elif choice == "3":
-        # 处理无效字符
-        try:
-            task_number = int(input("请输入要完成的任务编号："))
-        except ValueError:
-            print("请输入有效的任务数字编号!!!")
+        index = get_task_index(tasks, "完成")
+        if index is None:
             continue
-        
-        # 处理无效数字
-        if task_number < 1 or task_number > len(tasks):
-            print("没有这个任务编号!!!")
-            continue
-        
-        # 在列表内索引减一
-        index = task_number - 1
-        
         # 标记任务为已完成
         tasks[index]["done"] = True
     
@@ -83,21 +89,12 @@ while True:
         save_tasks(tasks)
             
         print("任务已完成。")
+
+    # 删除任务
     elif choice == "4":
-        # 处理无效字符
-        try:
-            task_number = int(input("请输入要删除的任务编号："))
-        except ValueError:
-            print("请输入有效的任务数字编号!!!")
+        index = get_task_index(tasks, "删除")
+        if index is None:
             continue
-        
-        # 处理无效数字
-        if task_number < 1 or task_number > len(tasks):
-            print("没有这个任务编号!!!")
-            continue
-        
-        index = task_number - 1
-        
         # 删除任务
         removed_task = tasks.pop(index)
         
